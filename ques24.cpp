701022c7d2c3d43dbe488edcfae9909c2f47d733
yuvraj717 <y0877282@gmail.com>
2026-05-09 21:18:29 +0530

    Add pattern printing program in ques24.cpp
    Implement a program to print a pattern of letters.

---
+++ ques24.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;   // Number of rows
+
+    for(int i = 1; i <= n; i++) {
+        for(char ch = 'A'; ch < 'A' + i; ch++) {
+            cout << ch;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

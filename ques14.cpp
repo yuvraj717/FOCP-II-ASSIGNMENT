f6477c3f75859c807470c2fe9d0ea2e4f9b372fa
yuvraj717 <y0877282@gmail.com>
2026-05-09 21:11:28 +0530

    Implement basic calculator with menu options

---
+++ ques14.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice;
+    double num1, num2, result;
+
+    do {
+        // Display menu
+        cout << "\n--- Menu ---" << endl;
+        cout << "1. Addition" << endl;
+        cout << "2. Subtraction" << endl;
+        cout << "3. Multiplication" << endl;
+        cout << "4. Division" << endl;
+        cout << "5. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if(choice >= 1 && choice <= 4) {
+            cout << "Enter first number: ";
+            cin >> num1;
+            cout << "Enter second number: ";
+            cin >> num2;
+        }
+
+        switch(choice) {
+            case 1:
+                result = num1 + num2;
+                cout << "Result: " << result << endl;
+                break;
+            case 2:
+                result = num1 - num2;
+                cout << "Result: " << result << endl;
+                break;
+            case 3:
+                result = num1 * num2;
+                cout << "Result: " << result << endl;
+                break;
+            case 4:
+                if(num2 != 0) {
+                    result = num1 / num2;
+                    cout << "Result: " << result << endl;
+                } else {
+                    cout << "Error: Division by zero is not allowed." << endl;
+                }
+                break;
+            case 5:
+                cout << "Exiting program..." << endl;
+                break;
+            default:
+                cout << "Invalid choice! Please try again." << endl;
+        }
+
+    } while(choice != 5);
+
+    return 0;
+}
